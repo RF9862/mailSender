e3d70923f7871fe3726ad30952736da54a791061
RF9862 <que.quao126@gmail.com>
2024-10-01 01:57:52 -0700

    get_request

--- mailSender.js
+++ mailSender.js
@@ -43,6 +43,10 @@ app.post('/verify-email', (req, res) => {
     // });
 });
 
+app.get('/', (req, res) => {
+    res.send('Hello world');
+});
+
 // Start the server
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
